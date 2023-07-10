21019957e853189921b6ea047eb346ab449ef7bc
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-07-10 22:11:33 +0900

    Create 가장_긴_증가하는_부분_수열.py

---
+++ dynamic_programming/가장_긴_증가하는_부분_수열.py
@@ -0,0 +1,11 @@
+N = int(input())
+arr = list(map(int, input().split()))
+ 
+dp = [1] * N
+
+for i in range(1, N):
+    for j in range(0, i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+    
+print(max(dp))
