00d02644cdd3c1e07580ea1dd64c8070a46becdb
yevini118 <79689350+yevini118@users.noreply.github.com>
2023-08-20 01:30:48 +0900

    Update 스티커.py

--- dynamic_programming/스티커.py
+++ dynamic_programming/스티커.py
@@ -2,14 +2,21 @@ T = int(input())
 
 def dp():
 
-    for y in range(2, n):
-        for x in range(2):
-            mem[x].append(sticker[x][y] + max(max(mem[x][:y-1]), max(mem[x-1][:y])))
+    #memory
+    mem = [[0, sticker[i][0]] for i in range(2)]
+
+    for y in range(1, n):
+
+        #점화식 : 최대점수 = 자기점수 + max(한칸대각선 점수 , 두칸 대각선 점수)
+        mem[0].append(sticker[0][y] + max(mem[1][-1], mem[1][-2]))  #윗줄
+        mem[1].append(sticker[1][y] + max(mem[0][-2], mem[0][-3]))  #아래줄
+
+    return max(mem[0][-1], mem[1][-1])
                 
 for i in range(T):
+
+    #input
     n = int(input())
     sticker = [list(map(int, input().split())) for _ in range(2)]
-    mem = [[sticker[0][0], sticker[0][1] + sticker[1][0]], [sticker[1][0], sticker[1][1] + sticker[0][0]]]
 
-    dp()
-    print(max(mem[0][n-1], mem[1][n-1]))
+    print(dp())
